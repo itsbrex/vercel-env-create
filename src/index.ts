ee40fb497516ff583fff7d591926a31681014ab0
itsbrex <roach7@gmail.com>
2023-03-16 23:39:48 -0700

    Update index.ts to fix typo in shebang line and disable eslint for unused variable. Refactor createEnvFiles and appendEnvVariables functions to return promises and use try-catch blocks for error handling

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-#!/usr/bin/env node
+#!/usr/bin / env node
 
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import * as fs from 'node:fs'
 import * as path from 'node:path'
 
@@ -59,69 +60,25 @@ for (const example of exampleFiles) {
 }
 
 if (existingFiles.length > 0) {
-  console.log('\n🚨 The following .env files already exist in the current directory: ')
-  for (const filePath of existingFiles) {
-    const fileName = path.basename(filePath)
-    console.log(`  - ${fileName}`)
-  }
-
-  console.log('\nAppending new environment variables if any...')
-
   appendEnvVariables()
-    .then(() => console.log('\nEnvironment variables appended successfully! 🙌'))
-    .catch((err) => console.error(err))
 } else {
   createEnvFiles()
-    .then(() => console.log('\nEnvironment files created successfully 🙌'))
-    .catch((err) => console.error(err))
 }
 
-export async function createEnvFiles(): Promise<void> {
-  searchDirectory(currentDirectory)
+export function createEnvFiles(): Promise<void> {
+  return new Promise<void>((resolve, reject) => {
+    try {
+      searchDirectory(currentDirectory)
 
-  for (const suffix of suffixes) {
-    const envFilePath = path.join(currentDirectory, `.env${suffix}`)
-    const envFileContent: string[] = []
+      for (const suffix of suffixes) {
+        const envFilePath = path.join(currentDirectory, `.env${suffix}`)
+        const envFileContent: string[] = []
 
-    if (suffix !== '') {
-      envFileContent.push(`ENV_FILE=.env${suffix}`)
-    }
+        if (suffix !== '') {
+          envFileContent.push(`ENV_FILE=.env${suffix}`)
+        }
 
-    for (const envVar of envVars[suffix] ?? []) {
-      if (suffix === '' && envVar === 'ENV_FILE') {
-        continue
-      } else if (suffix !== '' && envVar === 'ENV_FILE') {
-        envFileContent.push(`${envVar}=.${suffix}`)
-      } else {
-        envFileContent.push(`${envVar}=`)
-      }
-    }
-
-    fs.writeFileSync(envFilePath, envFileContent.join('\n'))
-
-    if (suffix === '') {
-      console.log('✅  - .env file created')
-    } else {
-      console.log(`✅  - .env${suffix} file created`)
-    }
-  }
-
-  console.log('\nEnvironment files created successfully 🙌')
-}
-
-export async function appendEnvVariables(): Promise<void> {
-  searchDirectory(currentDirectory)
-
-  for (const suffix of suffixes) {
-    const envFilePath = path.join(currentDirectory, `.env${suffix}`)
-    const envFileContent: string[] = []
-
-    if (fs.existsSync(envFilePath)) {
-      const existingContent = fs.readFileSync(envFilePath, 'utf8').split('\n')
-      const existingVars = new Set(existingContent.map((line) => line.split('=')[0]))
-
-      for (const envVar of envVars[suffix] ?? []) {
-        if (!existingVars.has(envVar)) {
+        for (const envVar of envVars[suffix] ?? []) {
           if (suffix === '' && envVar === 'ENV_FILE') {
             continue
           } else if (suffix !== '' && envVar === 'ENV_FILE') {
@@ -130,19 +87,51 @@ export async function appendEnvVariables(): Promise<void> {
             envFileContent.push(`${envVar}=`)
           }
         }
+
+        fs.writeFileSync(envFilePath, envFileContent.join('\n'))
       }
 
-      if (envFileContent.length > 0) {
-        fs.appendFileSync(envFilePath, envFileContent.join('\n') + '\n')
+      resolve()
+    } catch (error) {
+      reject(error)
+    }
+  })
+}
 
-        if (suffix === '') {
-          console.log('✅  - .env file updated')
-        } else {
-          console.log(`✅  - .env${suffix} file updated`)
+export function appendEnvVariables(): Promise<void> {
+  return new Promise<void>((resolve, reject) => {
+    try {
+      searchDirectory(currentDirectory)
+
+      for (const suffix of suffixes) {
+        const envFilePath = path.join(currentDirectory, `.env${suffix}`)
+        const envFileContent: string[] = []
+
+        if (fs.existsSync(envFilePath)) {
+          const existingContent = fs.readFileSync(envFilePath, 'utf8').split('\n')
+          const existingVars = new Set(existingContent.map((line) => line.split('=')[0]))
+
+          for (const envVar of envVars[suffix] ?? []) {
+            if (!existingVars.has(envVar)) {
+              if (suffix === '' && envVar === 'ENV_FILE') {
+                continue
+              } else if (suffix !== '' && envVar === 'ENV_FILE') {
+                envFileContent.push(`${envVar}=.${suffix}`)
+              } else {
+                envFileContent.push(`${envVar}=`)
+              }
+            }
+          }
+
+          if (envFileContent.length > 0) {
+            fs.appendFileSync(envFilePath, `${envFileContent.join('\n')}\n`)
+          }
         }
       }
-    }
-  }
 
-  console.log('\nEnvironment variables appended successfully 🙌')
+      resolve()
+    } catch (error) {
+      reject(error)
+    }
+  })
 }

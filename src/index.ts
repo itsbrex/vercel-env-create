1436f692ef216d864aab2e922bab624429f22456
itsbrex <roach7@gmail.com>
2023-03-16 22:59:20 -0700

    updated to use async + promises

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,10 @@
 
 import * as fs from 'node:fs'
 import * as path from 'node:path'
-import * as readline from 'node:readline'
 
 const pattern = /process.env\.(\w+)/g
 
-const suffixes = ['', '.local', '.development', '.production']
+const suffixes = ['', '.example', '.local', '.development', '.production']
 
 const envVars: Record<string, Set<string>> = {}
 for (const suffix of suffixes) {
@@ -43,7 +42,21 @@ function searchDirectory(directory: string): void {
 const currentDirectory = process.cwd()
 
 const envFilePaths = suffixes.map((suffix) => path.join(currentDirectory, `.env${suffix}`))
+
+const exampleFiles = ['.env.example', '.env.local.example'].map((example) => path.join(currentDirectory, example))
+
 const existingFiles = envFilePaths.filter(fs.existsSync)
+
+for (const example of exampleFiles) {
+  if (fs.existsSync(example)) {
+    for (const filePath of envFilePaths) {
+      if (!fs.existsSync(filePath)) {
+        fs.copyFileSync(example, filePath)
+      }
+    }
+    break
+  }
+}
 
 if (existingFiles.length > 0) {
   console.log('\n🚨 The following .env files already exist in the current directory: ')
@@ -52,41 +65,18 @@ if (existingFiles.length > 0) {
     console.log(`  - ${fileName}`)
   }
 
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  })
+  console.log('\nAppending new environment variables if any...')
 
-  rl.question(
-    '\nDo you want to:\n1. Delete existing .env files and create new ones\n2. Cancel and exit\n\nEnter your choice (1-2): ',
-    (choice: string) => {
-      if (choice === '1') {
-        for (const filePath of existingFiles) fs.unlinkSync(filePath)
-        console.log('\n❌ Deleted existing .env files\n')
-      } else if (choice === '2') {
-        console.log('\nExiting...👋\n')
-        process.exit(1)
-      } else {
-        console.log('\nInvalid choice. 🤦 Exiting...\n')
-        process.exit(1)
-      }
-
-      rl.close()
-
-      createEnvFiles()
-
-      console.log('\nEnvironment files created successfully! 🙌')
-      process.exit()
-    }
-  )
+  appendEnvVariables()
+    .then(() => console.log('\nEnvironment variables appended successfully! 🙌'))
+    .catch((err) => console.error(err))
 } else {
   createEnvFiles()
-
-  console.log('\nEnvironment files created successfully 🙌')
-  process.exit()
+    .then(() => console.log('\nEnvironment files created successfully 🙌'))
+    .catch((err) => console.error(err))
 }
 
-function createEnvFiles() {
+export async function createEnvFiles(): Promise<void> {
   searchDirectory(currentDirectory)
 
   for (const suffix of suffixes) {
@@ -117,5 +107,42 @@ function createEnvFiles() {
   }
 
   console.log('\nEnvironment files created successfully 🙌')
-  process.exit()
+}
+
+export async function appendEnvVariables(): Promise<void> {
+  searchDirectory(currentDirectory)
+
+  for (const suffix of suffixes) {
+    const envFilePath = path.join(currentDirectory, `.env${suffix}`)
+    const envFileContent: string[] = []
+
+    if (fs.existsSync(envFilePath)) {
+      const existingContent = fs.readFileSync(envFilePath, 'utf8').split('\n')
+      const existingVars = new Set(existingContent.map((line) => line.split('=')[0]))
+
+      for (const envVar of envVars[suffix] ?? []) {
+        if (!existingVars.has(envVar)) {
+          if (suffix === '' && envVar === 'ENV_FILE') {
+            continue
+          } else if (suffix !== '' && envVar === 'ENV_FILE') {
+            envFileContent.push(`${envVar}=.${suffix}`)
+          } else {
+            envFileContent.push(`${envVar}=`)
+          }
+        }
+      }
+
+      if (envFileContent.length > 0) {
+        fs.appendFileSync(envFilePath, envFileContent.join('\n') + '\n')
+
+        if (suffix === '') {
+          console.log('✅  - .env file updated')
+        } else {
+          console.log(`✅  - .env${suffix} file updated`)
+        }
+      }
+    }
+  }
+
+  console.log('\nEnvironment variables appended successfully 🙌')
 }
